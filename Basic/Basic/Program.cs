e0ff31d0f40a7e87c46147d806d5887e7fdff452
Bruno Tassinari <bruno.fagundes80@gmail.com>
2023-05-15 11:16:57 -0300

    restructure folder

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Press enter to exit...");
+        Console.ReadLine();
+    }
+}
---
+++ Basic/Texts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("char variables");
+
+        char letter;
+        letter = 'a';
+        Console.WriteLine("The letter is " + letter);
+
+        letter = (char)(letter + 1);
+        // char prints b, not 98 ASCII
+        Console.WriteLine("The letter is " + letter);
+
+        letter++;
+        // char prints c, not 99 ASCII
+        Console.WriteLine("The letter is " + letter);
+
+
+
+        Console.WriteLine();
+        Console.WriteLine("Press enter to exit...");
+        Console.ReadLine();
+    }
+}
