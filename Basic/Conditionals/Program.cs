0ef3d4034b9d97509b5179f3aeacd3c7fd238619
Bruno Tassinari <bruno.fagundes80@gmail.com>
2023-05-16 08:35:52 -0300

    add conditionals

---
+++ Basic/Conditionals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("conditionals");
+        Console.WriteLine();
+
+        Console.WriteLine("conditionals");
+        
+        int age = 16;
+
+        if(age >= 18)
+        {
+            Console.WriteLine("You are an adult");
+        }
+        else
+        {
+            Console.WriteLine("You are not an adult");
+        }
+
+
+
+
+
+        Console.WriteLine();
+        Console.WriteLine("Press enter to exit...");
+        Console.ReadLine();
+    }
+}
