3ddce244f62276f910994758e0d2aa37ab3ab424
Bruno Tassinari <bruno.fagundes80@gmail.com>
2023-05-24 01:17:19 -0300

    init api

---
+++ ASP.NET/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController : ControllerBase
+{
+
+    private static List<Movie> _movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        _movies.Add(movie);
+    }
+    
+    [HttpGet]
+    public List<Movie> GetMovies()
+    {
+        return _movies;
+    }
+    
+    [HttpGet("{year}")]
+    public List<Movie> GetMoviesByYear(int year)
+    {
+        return _movies.Where(movie => movie.Year == year).ToList();
+    }
+
+}
---
+++ ASP.NET/MoviesAPI/Entities/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesAPI.Entities;
+
+public class Movie
+{
+    public string Title { get; set; } = "";
+    public string Director { get; set; } = "";
+    public int Year { get; set; }
+}
